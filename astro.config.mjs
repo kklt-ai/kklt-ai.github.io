46428be6f5fdb447aca2f0d56ba0eb3d49940f40
kklt <bigjuy@163.com>
2026-07-31 22:49:16 +0800

    优化

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://kklt-ai.github.io',
+  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'vitesse-light',
